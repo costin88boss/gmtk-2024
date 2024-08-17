46c5f976686d38a25ed1267fe7c081fc552a67b1
Pixels <164868626+Pixels75@users.noreply.github.com>
2024-08-17 22:17:35 +0300

    Added Save/Load Functionality for Text

---
+++ game/src/loadsavetxt.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <cmath>
+
+std::string LoadStringFromFile( const std::string& filePath );
+void SaveStringToFile( const std::string& string, const std::string& filePath );
+Text LoadTextDataFromFile( const std::string& filePath );
+Text LoadTextDataFromString( const std::string string );
+std::string ParseStringForTag( const std::string& string, const std::string& tag );
+
+std::string LoadStringFromFile( const std::string& filePath )
+{
+    std::ifstream file( filePath );
+    std::string string;
+    while ( getline( file, string ) ) {}
+
+    file.close();
+    
+    return string;
+}
+
+void SaveStringToFile( const std::string& string, const std::string& filePath )
+{
+    std::ofstream file;
+
+    file.open( filePath, std::ios_base::app);
+    file << string; 
+
+    file.close();
+}
+
+Text LoadTextDataFromFile( const std::string& filePath )
+{
+    std::string data = LoadStringFromFile( filePath );
+    Text text = LoadTextDataFromString( data );
+
+    return text;
+}
+
+Text LoadTextDataFromString( const std::string string )
+{
+    std::string outText;
+    Vector2 outPos;
+    int outSize;
+    Color outColor;
+
+    std::string textStr = ParseStringForTag( string, "text" );
+    std::string posXStr = ParseStringForTag( string, "posX" );
+    std::string posYStr = ParseStringForTag( string, "posY" );
+    std::string sizeStr = ParseStringForTag( string, "size" );
+    std::string colorRStr = ParseStringForTag( string, "colorR" );
+    std::string colorGStr = ParseStringForTag( string, "colorG" );
+    std::string colorBStr = ParseStringForTag( string, "colorB" );
+    std::string colorAStr = ParseStringForTag( string, "colorA" );
+
+    outText = textStr;
+
+    outPos.x = std::stof( posXStr );
+    outPos.y = std::stof( posYStr );
+
+    outSize = std::stoi( sizeStr );
+
+    outColor.r = std::stoi( colorRStr );
+    outColor.g = std::stoi( colorGStr );
+    outColor.b = std::stoi( colorBStr );
+    outColor.a = std::stoi( colorAStr );
+
+    Text out( outPos, outText, outSize, outColor );
+
+    return out;
+}
+
+std::string ParseStringForTag( const std::string& string, const std::string& tag )
+{
+    std::string out;
+    std::string buffer;
+
+    for ( int i = 0; i < string.length(); i++ )
+    {
+        buffer += string.at( i );
+
+        if ( buffer == ( tag + ":" ) )
+        {
+            i++;
+            while ( string.at( i ) != '\n' )
+            {
+                out += string.at( i );
+                i++;
+            }
+
+            return out;
+        }
+        else if ( string.at( i ) == '\n' )
+        {
+            buffer.clear();
+        }
+    }
+
+    return "";
+}
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include "raylib.h"
+#include "text.hpp"
+#include "loadsavetxt.hpp"
 
 int main(void)
 {
@@ -7,24 +10,36 @@ int main(void)
 
     InitAudioDevice();
 
+    // Text( { 220, 270 }, "HELLO, WORLD!", 50, WHITE );
+    std::string data = "text:HELLO, WORLD!\n"
+                       "posX:220.0\n"
+                       "posY:270.0\n"
+                       "size:50\n"
+                       "colorR:255\n"
+                       "colorG:255\n"
+                       "colorB:255\n"
+                       "colorA:255\n";
     
+    Text title = LoadTextDataFromString(data);
 
     while (!WindowShouldClose())
     {
         ClearBackground(RED);
 
-
         BeginDrawing();
 
         DrawFPS(0, 0);
+        DrawRectangleGradientV( 300, 200, 200, 200, BLUE, GREEN);
 
-        DrawRectangleGradientV(200, 200, 200, 200, BLUE, GREEN);
+        DrawText( title.text.c_str(),
+                  title.position.x,
+                  title.position.y, 
+                  title.size, 
+                  title.color );
 
         EndDrawing();
     }
 
     CloseAudioDevice();
     CloseWindow();
-
-    return 0;
 }
