7ba71299d20a226f504e692e670d1c291a780bd4
Pixels <164868626+Pixels75@users.noreply.github.com>
2024-08-18 02:45:23 +0300

    Small Improvements

--- game/src/loadsavetxt.hpp
+++ game/src/loadsavetxt.hpp
@@ -2,12 +2,11 @@
 
 #include <fstream>
 #include <string>
-#include <cmath>
 
 std::string LoadStringFromFile( const std::string& filePath );
 void SaveStringToFile( const std::string& string, const std::string& filePath );
 Text LoadTextDataFromFile( const std::string& filePath );
-Text LoadTextDataFromString( const std::string string );
+Text LoadTextDataFromString( const std::string& string );
 std::string ParseStringForTag( const std::string& string, const std::string& tag );
 
 std::string LoadStringFromFile( const std::string& filePath )
@@ -39,7 +38,7 @@ Text LoadTextDataFromFile( const std::string& filePath )
     return text;
 }
 
-Text LoadTextDataFromString( const std::string string )
+Text LoadTextDataFromString( const std::string& string )
 {
     std::string outText;
     Vector2 outPos;
