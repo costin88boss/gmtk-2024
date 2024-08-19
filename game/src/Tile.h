974fced9ecd870bd6b28c564da5dd097af412399
Pixels <164868626+Pixels75@users.noreply.github.com>
2024-08-19 05:34:51 +0300

    Minor Changes

--- game/src/Tile.h
+++ game/src/Tile.h
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <raylib.h>
+#include "Entities/Gate.h"
+#include "Entities/Plate.h"
+#include "Entities/Movable.h"
 
 struct Tile {
 	//Texture2D* bgTexture;
 	int test;
 	// TO-DO
 	// LevelEntity primaryEntity; --- pressure plate/gate
+
+	Gate gate;  // an open gate could be used for no gate ( aka null gate )
+	Plate plate;// a plate with m_size = 0 and m_gate = nullptr could be used for no plate ( aka null plate )
+	
 	// PhysicalEntity secondaryEntity; --- player/crate
+
+	Movable* movableObj; // nullptr for no movableobj
 	// or smth like that
 	
 };
